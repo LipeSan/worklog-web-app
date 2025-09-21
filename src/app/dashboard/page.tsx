8df827dda74e878adebf877c069dbe9f04d90afd
Filipe Ferandes <flpsan@gmail.com>
2025-09-21 19:48:05 +1000

    FIX - fixed to deploy

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -11,8 +11,6 @@ import {
   Plus,
   Loader2,
   AlertCircle,
-  Edit,
-  Trash2,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
